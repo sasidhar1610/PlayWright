e650e2535af2cefb2e7a6bb8ffdbb88209b424ae
sasidhar1610 <sasidhar1610@gmail.com>
2026-06-12 22:58:10 +0530

    new commit jun04

---
+++ 1BasicJavascriptTypescriptPrograms/3_Arrays/Array1.ts
@@ -0,0 +1,13 @@
+let std1marks : number = 78;
+let std2marks : number = 56;
+let std3marks : number = 13;
+let std4marks : number = 98;
+let std5marks : number = 41;
+let std6marks : number = 66;
+let std7marks : number = 81;
+let std8marks : number = 37;
+
+console.log(std1marks)
+
+let stdmarks : numbers[] = [ 78, 56, 13, 98, 78, 56, 64, 38];
+console.log(stdmarks);
